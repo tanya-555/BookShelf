5dd45f1a175599b0d61e5f6fd0bed99b3be0cb7c
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-04 00:20:04 +0530

    set orientation for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:bookshelf/screens/landing_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(BookShelfApp());
 }
 
@@ -12,6 +15,7 @@ class BookShelfApp extends StatelessWidget {
       title: "Book Shelf",
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        accentColor: Colors.deepPurpleAccent,
       ),
       home: LandingScreen(),
     );
