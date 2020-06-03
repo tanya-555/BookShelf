c53dc9b0b0109d3f95500bc4fc817da0b3b97567
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-03 22:36:18 +0530

    create landing screen widget

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookshelf/widgets/landing_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BookShelfApp());
+}
+
+class BookShelfApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Book Shelf",
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: LandingScreen(),
+    );
+  }
+}
