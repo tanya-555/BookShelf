5d8489f79c73891b94081ad43eb093b5aed85d03
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-04 15:33:43 +0530

    add category id in category model

--- lib/data/category_data.dart
+++ lib/data/category_data.dart
@@ -1,10 +1,10 @@
 import 'package:bookshelf/model/category_model.dart';
 
 const CATEGORY_DATA = const [
-  Category(name: "Fiction", image: "assets/images/book1.jpg"),
-  Category(name: "Non Fiction", image: "assets/images/book2.jpg"),
-  Category(name: "Crime Thriller", image: "assets/images/book3.jpg"),
-  Category(name: "Classics", image: "assets/images/book4.jpg"),
-  Category(name: "Motivation", image: "assets/images/book5.jpg"),
-  Category(name: "Mythology", image: "assets/images/book6.jpg"),
+  Category(name: "Fiction", image: "assets/images/book1.jpg", id: 1),
+  Category(name: "Non Fiction", image: "assets/images/book2.jpg", id: 2),
+  Category(name: "Crime Thriller", image: "assets/images/book3.jpg", id: 3),
+  Category(name: "Classics", image: "assets/images/book4.jpg", id: 4),
+  Category(name: "Motivation", image: "assets/images/book5.jpg", id: 5),
+  Category(name: "Mythology", image: "assets/images/book6.jpg", id: 6),
 ];
--- lib/model/category_model.dart
+++ lib/model/category_model.dart
@@ -3,9 +3,11 @@ import 'package:flutter/cupertino.dart';
 class Category {
   final String name;
   final String image;
+  final int id;
 
   const Category({
     @required this.name,
     @required this.image,
+    @required this.id,
   });
 }
