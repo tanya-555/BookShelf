4ab7aecff60722a7f7ec1af7a04a2fb5d035e877
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-04 19:23:27 +0530

    format book list item

--- lib/widgets/book_item.dart
+++ lib/widgets/book_item.dart
@@ -11,33 +11,61 @@ class BookItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(5),
+      child: Container(
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-        ),
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10),
+              topLeft: Radius.circular(10),
+            ),
+            border: Border.all(
+              color: Colors.black,
+            )),
         child: Column(
           children: <Widget>[
-            Container(
-              width: 500,
-              height: 400,
-              decoration: BoxDecoration(
-                image: new DecorationImage(
-                  image: AssetImage(
-                    model.image,
+            Stack(
+              children: <Widget>[
+                Container(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(10),
+                        topLeft: Radius.circular(10)),
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: MediaQuery.of(context).size.height * 0.5,
+                      decoration: BoxDecoration(
+                        image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage(model.image),
+                        ),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+              ],
             ),
             Container(
-              child: Text(
-                model.title,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                ),
+              width: MediaQuery.of(context).size.width * 1,
+              height: MediaQuery.of(context).size.height * 0.1,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      model.title,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
