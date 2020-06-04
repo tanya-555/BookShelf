b853a647b33df6ff6574f051f0352af05364b8a1
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-04 19:26:06 +0530

    remove unused widgets from widget tree

--- lib/widgets/book_item.dart
+++ lib/widgets/book_item.dart
@@ -23,44 +23,36 @@ class BookItem extends StatelessWidget {
             )),
         child: Column(
           children: <Widget>[
-            Stack(
-              children: <Widget>[
-                Container(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(10),
-                        topLeft: Radius.circular(10)),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 1,
-                      height: MediaQuery.of(context).size.height * 0.5,
-                      decoration: BoxDecoration(
-                        image: new DecorationImage(
-                          fit: BoxFit.fill,
-                          image: AssetImage(model.image),
-                        ),
-                      ),
+            Container(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                    topLeft: Radius.circular(10)),
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 1,
+                  height: MediaQuery.of(context).size.height * 0.5,
+                  decoration: BoxDecoration(
+                    image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(model.image),
                     ),
                   ),
                 ),
-              ],
+              ),
             ),
             Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(10),
               width: MediaQuery.of(context).size.width * 1,
               height: MediaQuery.of(context).size.height * 0.1,
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    child: Text(
-                      model.title,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontStyle: FontStyle.italic,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
+              child: Text(
+                model.title,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ],
