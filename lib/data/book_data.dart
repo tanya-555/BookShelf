ea6e147a3aa7ebd0b32d724e44ec8757714ce08b
tanya-555 <52540265+tanya-555@users.noreply.github.com>
2020-06-08 19:49:40 +0530

    add summary data for all book objects

--- lib/data/book_data.dart
+++ lib/data/book_data.dart
@@ -9,6 +9,8 @@ const BOOK_DATA = const [
       pages: 328,
       language: "English",
       categories: [1],
+      summary:
+          "Among the seminal texts of the 20th century, Nineteen Eighty-Four is a rare work that grows more haunting as its futuristic purgatory becomes more real. Published in 1949, the book offers political satirist George Orwell's nightmarish vision of a totalitarian, bureaucratic world and one poor stiff's attempt to find individuality. The brilliance of the novel is Orwell's prescience of modern life—the ubiquity of television, the distortion of the language—and his ability to construct such a thorough version of hell. Required reading for students since it was published, it ranks among the most terrifying novels ever written.",
       image: "assets/images/image_1984.jpg"),
   BookModel(
       title: "To Kill A Mockingbird",
@@ -18,6 +20,8 @@ const BOOK_DATA = const [
       pages: 320,
       language: "English",
       categories: [1, 4],
+      summary:
+          "The unforgettable novel of a childhood in a sleepy Southern town and the crisis of conscience that rocked it, To Kill A Mockingbird became both an instant bestseller and a critical success when it was first published in 1960. It went on to win the Pulitzer Prize in 1961 and was later made into an Academy Award-winning film, also a classic.",
       image: "assets/images/to_kill_mockingbird.jpg"),
   BookModel(
       title: "The Great Gatsby",
@@ -27,6 +31,8 @@ const BOOK_DATA = const [
       pages: 240,
       language: "English",
       categories: [1, 4],
+      summary:
+          "The Great Gatsby, F. Scott Fitzgerald's third book, stands as the supreme achievement of his career. This exemplary novel of the Jazz Age has been acclaimed by generations of readers. The story is of the fabulously wealthy Jay Gatsby and his new love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted \"gin was the national drink and sex the national obsession,\" it is an exquisitely crafted tale of America in the 1920s.",
       image: "assets/images/great_gatsby.jpg"),
   BookModel(
       title: "Great Expectations",
@@ -36,6 +42,8 @@ const BOOK_DATA = const [
       pages: 544,
       language: "English",
       categories: [1, 4],
+      summary:
+          "In what may be Dickens's best novel, humble, orphaned Pip is apprenticed to the dirty work of the forge but dares to dream of becoming a gentleman — and one day, under sudden and enigmatic circumstances, he finds himself in possession of \"great expectations.\" In this gripping tale of crime and guilt, revenge and reward, the compelling characters include Magwitch, the fearful and fearsome convict; Estella, whose beauty is excelled only by her haughtine",
       image: "assets/images/great_expectations.jpg"),
   BookModel(
       title: "Midnight's Children",
@@ -45,6 +53,8 @@ const BOOK_DATA = const [
       pages: 672,
       language: "English",
       categories: [1, 4],
+      summary:
+          "Midnight's Children is a 1981 novel by Indian author Salman Rushdie. It deals with India's transition from British colonialism to independence and the partition of India. It is considered an example of postcolonial, postmodern, and magical realist literature. The story is told by its chief protagonist, Saleem Sinai, and is set in the context of actual historical events. The style of preserving history with fictional accounts is self-reflexive.",
       image: "assets/images/midnight_children.jpg"),
   BookModel(
       title: "The Count of Monte Cristo",
@@ -54,6 +64,8 @@ const BOOK_DATA = const [
       pages: 1312,
       language: "English",
       categories: [1, 4],
+      summary:
+          "Thrown in prison for a crime he has not committed, Edmond Dantes is confined to the grim fortress of If. There he learns of a great hoard of treasure hidden on the Isle of Monte Cristo and he becomes determined not only to escape, but also to unearth the treasure and use it to plot the destruction of the three men responsible for his incarceration. Dumas’ epic tale of suffering and retribution, inspired by a real-life case of wrongful imprisonment, was a huge popular success when it was first serialized in the 1840s.",
       image: "assets/images/count_monte_cristo.jpg"),
   BookModel(
       title: "Gone Girl",
@@ -63,6 +75,8 @@ const BOOK_DATA = const [
       pages: 432,
       language: "English",
       categories: [1, 3],
+      summary:
+          "On a warm summer morning in North Carthage, Missouri, it is Nick and Amy Dunne’s fifth wedding anniversary. Presents are being wrapped and reservations are being made when Nick’s clever and beautiful wife disappears from their rented McMansion on the Mississippi River. Husband-of-the-Year Nick isn’t doing himself any favors with cringe-worthy daydreams about the slope and shape of his wife’s head, but passages from Amy's diary reveal the alpha-girl perfectionist could have put anyone dangerously on edge. Under mounting pressure from the police and the media—as well as Amy’s fiercely doting parents—the town golden boy parades an endless series of lies, deceits, and inappropriate behavior. Nick is oddly evasive, and he’s definitely bitter—but is he really a killer?",
       image: "assets/images/gone_girl.jpg"),
   BookModel(
       title: "The Widow",
@@ -72,6 +86,8 @@ const BOOK_DATA = const [
       pages: 324,
       language: "English",
       categories: [1, 3],
+      summary:
+          "When the police started asking questions, Jean Taylor turned into a different woman. One who enabled her and her husband to carry on, when more bad things began to happen...",
       image: "assets/images/widow.jpeg"),
   BookModel(
       title: "Strangers",
@@ -81,6 +97,8 @@ const BOOK_DATA = const [
       pages: 526,
       language: "English",
       categories: [1, 3],
+      summary:
+          "Six strangers are unaccountably seized by nightmares, attacks of fear, and bouts of uncharacteristic behavior. The six begin to seek each other out as puzzling photographs and messages arrive, indicating that the cause may lie in a forgotten weekend stay at an isolated Nevada motel. Koontz has topped a fine roster of horror and suspense novels with an almost unbearably suspenseful page-turner. His ability to maintain the mystery through several plot twists is impressive, as is his array of believable and sympathetic characters. With its masterful blend of elements of espionage, terror, and even some science fiction, Strangers may be the suspense novel of the year.",
       image: "assets/images/strangers.jpg"),
   BookModel(
       title: "The Girl on the Train",
@@ -90,6 +108,8 @@ const BOOK_DATA = const [
       pages: 416,
       language: "English",
       categories: [1, 3],
+      summary:
+          "Rachel catches the same commuter train every morning. She knows it will wait at the same signal each time, overlooking a row of back gardens. She’s even started to feel like she knows the people who live in one of the houses. “Jess and Jason,” she calls them. Their life—as she sees it—is perfect. If only Rachel could be that happy. And then she sees something shocking. It’s only a minute until the train moves on, but it’s enough. Now everything’s changed. Now Rachel has a chance to become a part of the lives she’s only watched from afar. Now they’ll see; she’s much more than just the girl on the train",
       image: "assets/images/girl_on_train.jpeg"),
   BookModel(
       title: "The Immortals of Meluha",
@@ -99,6 +119,8 @@ const BOOK_DATA = const [
       pages: 390,
       language: "English, Hindi",
       categories: [1, 6],
+      summary:
+          "1900 BC. In what modern Indians mistakenly call the Indus Valley Civilisation. The inhabitants of that period called it the land of Meluha a near perfect empire created many centuries earlier by Lord Ram, one of the greatest monarchs that ever lived. This once proud empire and its Suryavanshi rulers face severe perils as its primary river, the revered Saraswati, is slowly drying to extinction. They also face devastating terrorist attacks from the east, the land of the Chandravanshis. To make matters worse, the Chandravanshis appear to have allied with the Nagas, an ostracised and sinister race of deformed humans with astonishing martial skills!The only hope for the Suryavanshis is an ancient legend: When evil reaches epic proportions, when all seems lost, when it appears that your enemies have triumphed, a hero will emerge.Is the rough-hewn Tibetan immigrant Shiva, really that hero? And does he want to be that hero at all? Drawn suddenly to his destiny, by duty as well as by love, will Shiva lead the Suryavanshi vengeance and destroy evil?",
       image: "assets/images/immortals_of_meluha.jpg"),
   BookModel(
       title: "The Secret of the Nagas",
@@ -108,6 +130,8 @@ const BOOK_DATA = const [
       pages: 396,
       language: "English, Hindi",
       categories: [1, 6],
+      summary:
+          "The hunt is on. The sinister Naga warrior has killed his friend Brahaspati and now stalks his wife Sati. Shiva, the Tibetan immigrant who is the prophesied destroyer of evil, will not rest till he finds his demonic adversary. His vengeance and the path to evil will lead him to the door of the Nagas, the serpent people. Of that he is certain.",
       image: "assets/images/secret_of_nagas.jpeg"),
   BookModel(
       title: "The Oath of the Vayuputras",
@@ -117,6 +141,8 @@ const BOOK_DATA = const [
       pages: 565,
       language: "English, Hindi",
       categories: [1, 6],
+      summary:
+          "Shiva is gathering his forces. He reaches the Naga capital, Panchavati, and Evil is finally revealed. The Neelkanth prepares for a holy war against his true enemy, a man whose name instils dread in the fiercest of warriors.",
       image: "assets/images/oath_of_vayuputras.jpg"),
   BookModel(
       title: "The Sea of Monsters",
@@ -126,6 +152,8 @@ const BOOK_DATA = const [
       pages: 279,
       language: "English",
       categories: [1, 6],
+      summary:
+          "The heroic son of Poseidon makes an action-packed comeback in the second must-read installment of Rick Riordan's amazing young readers series. Starring Percy Jackson, a \"half blood\" whose mother is human and whose father is the God of the Sea, Riordan's series combines cliffhanger adventure and Greek mythology lessons that results in true page-turners that get better with each installment.",
       image: "assets/images/sea_monsters.jpg"),
   BookModel(
       title: "Think and Grow Rich",
@@ -135,6 +163,8 @@ const BOOK_DATA = const [
       pages: 238,
       language: "English",
       categories: [2, 5],
+      summary:
+          "This is the original 1937 version of Napoleon Hill's Classic Book: Think and Grow Rich. To the greatest extent possible, the text and formatting have been kept exactly the same as in the original release with the exception of some minor formatting changes.",
       image: "assets/images/thin_grow_rich.jpg"),
   BookModel(
       title: "Ego is the Enemy",
@@ -144,6 +174,8 @@ const BOOK_DATA = const [
       pages: 256,
       language: "English",
       categories: [2, 5],
+      summary:
+          "The Ego is the Enemy draws on a vast array of stories and examples, from literature to philosophy to history. We meet fascinating figures like Howard Hughes, Katharine Graham, Bill Belichick, and Eleanor Roosevelt, all of whom reached the highest levels of power and success by conquering their own egos. Their strategies and tactics can be ours as well.",
       image: "assets/images/ego_enemy.jpg"),
   BookModel(
       title: "Rich Dad, Poor Dad",
@@ -153,6 +185,8 @@ const BOOK_DATA = const [
       pages: 207,
       language: "English",
       categories: [2, 5],
+      summary:
+          "Rich Dad Poor Dad is Robert's story of growing up with two dads — his real father and the father of his best friend, his \"rich dad\" — and the ways in which both men shaped his thoughts about money and investing. The book explodes the myth that you need to earn a high income to be rich and explains the difference between working for money and having your money work for you.",
       image: "assets/images/rich_poor.jpg"),
   BookModel(
       title: "The Secret",
@@ -162,5 +196,6 @@ const BOOK_DATA = const [
       pages: 198,
       language: "English",
       categories: [2, 5],
+      summary: "The Secret’s 10th Anniversary Edition includes a new foreword by Rhonda Byrne, and 10 of the most life-changing insights she’s had over the last 10 years of practicing and living The Secret every day. The 10 insights alone will accelerate your understanding and mastery of the law of attraction.",
       image: "assets/images/secret.jpg"),
 ];
